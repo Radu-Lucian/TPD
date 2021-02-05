1d966781134b5d9fe2049f4d748312c9c2c172fd
Lucian Radu <radulucian.andrei@gmail.com>
2021-02-05 20:58:14 +0200

    Added upload controller

--- mavenjavafx/src/main/java/WindowController/UploadFileController.java
+++ mavenjavafx/src/main/java/WindowController/UploadFileController.java
@@ -1,8 +1,24 @@
 package WindowController;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 public class UploadFileController extends BaseController {
+
+    @FXML
+    public Button selectFileButton;
+
+    @FXML
+    public Button uploadButton;
+
+    @FXML
+    public Label fileStatusLabel;
+
+    @FXML
+    public ListView<String> usersListView = new ListView<>();
 
     @FXML
     void initialize() {
@@ -10,5 +26,11 @@ public class UploadFileController extends BaseController {
 
     public void initData(String username, String allUsers) {
 
+    }
+
+    public void onSelectFileButtonClick(ActionEvent event) {
+    }
+
+    public void onUploadButtonClick(ActionEvent event) {
     }
 }
